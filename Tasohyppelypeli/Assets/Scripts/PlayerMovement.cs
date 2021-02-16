07a27660083f93c2dd8ab3587eb3af694c4cb59f
Robertsu24 <santeri.ristonen@gmail.com>
2021-02-16 19:31:08 +0200

    playermovement

---
+++ Tasohyppelypeli/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 2;
+    Rigidbody2D rb2D;
+    void Start()
+    {
+        // Start is called before the first frame update
+    }
+    void Update()
+    {
+        // Update is called once per frame
+    }
+}
