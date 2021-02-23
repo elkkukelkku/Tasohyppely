a6c7ba2f663ab74d4f193fae5e36411f614a3dab
Robertsu24 <santeri.ristonen@gmail.com>
2021-02-23 19:33:44 +0200

    mitä tähän pitää kirjoittaa?

--- Tasohyppelypeli/Assets/Scripts/PlayerMovement.cs
+++ Tasohyppelypeli/Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 2;
-    Rigidbody2D rb2D;
+    public Rigidbody2D rb2D;
+    private float horizontalMovement;
+
     void Start()
     {
         // Start is called before the first frame update
     }
+
     void Update()
     {
         // Update is called once per frame
+        horizontalMovement=Input.GetAxis("Horizontal");
+    }
+    private void FixedUpdate(){
+        Vector2 playerVelocity = new Vector2(horizontalMovement * moveSpeed, rb2D.velocity.y);
+        rb2D.velocity = playerVelocity;
     }
 }
